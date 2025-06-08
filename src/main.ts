5cbd6d18e2630025adc2daebd172df103f9dc190
Luisj950 <lajaramillo@sudamericano.edu.ec>
2025-06-08 02:13:40 -0500

    TERCER BACK

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://fronted-semanaa8-wd5e.vercel.app',
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
-  });
+  origin: 'https://fronted-semanaa8-wd5e.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // Solo si usas cookies o auth
+});
+
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
